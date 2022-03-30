d6b411c231ee23aefa3e8cfe3efd3a6c760942c4
Supriya11 <101583227+supriya5590@users.noreply.github.com>
2022-03-30 16:51:45 +0530

    Add files via upload

---
+++ Q1helloworld.java
@@ -0,0 +1,12 @@
+
+//Q 1 write a program to display Hello message
+
+package practiceASMT1;
+
+public class Q1helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
+	}
+
+}
